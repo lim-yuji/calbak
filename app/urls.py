44665ca78f3a3c2b773df74e44627bb121b3cd2a
lee-sangyun <power_brian@naver.com>
2025-05-19 15:07:01 +0900

    feat: 연속 일정 기능 추가 및 UI 개선

--- app/urls.py
+++ app/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     #path('google/login/',    views.google_login,    name='google_login'),
     #path('google/callback/', views.google_callback, name='google_callback'),
     #캘린더 페이지
-    #path('calendar/', views.calendar_view, name='calendar'),
-    #path('calendar/events/add/', views.add_event, name='add_event'),
-    #path('google/calendar/events/', views.get_holiday_events, name='get_holiday_events'),
+    path('calendar/', views.calendar_view, name='calendar'),
+    path('calendar/events/add/', views.add_event, name='add_event'),
+    path('google/calendar/events/', views.get_holiday_events, name='get_holiday_events'),
 ]
